2ec65d6ec05992fe843341ad4256b4d819e59f37
Julian Shi <yulin.shi.app@gmail.com>
2018-01-18 12:29:36 +0000

    server test file

---
+++ server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+import urlparse
+import subprocess
+
+message_json={
+ "messages": [
+   {"text": "Welcome to the Chatfuel Rockets!"},
+   {"text": "What are you up to?"}
+ ]
+}
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        parsed_path = urlparse.urlparse(self.path)
+        request_id = parsed_path.path
+        print(message_json)
+        # response = subprocess.check_output(["python", request_id])
+        # self.wfile.write(json.dumps(response))
+        self.wfile.write(json.dumps(message_json))
+
+    def do_POST(self):
+        self._set_headers()
+        parsed_path = urlparse.urlparse(self.path)
+        request_id = parsed_path.path
+        print("request_id of post")
+        print(request_id)
+        response = subprocess.check_output(["python", request_id])
+        self.wfile.write(json.dumps(response))
+
+    def do_HEAD(self):
+        self._set_headers()
+
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Starting httpd...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
